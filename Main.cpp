227de601abea8e03275f0d757a1c7774d3eecc2c
Shupperman <Jshup55@gmail.com>
2022-10-18 20:30:57 -0400

    Refactored code based on movement. Added the use of pointers.

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-// Version 1.10
+// Version 1.11
 
 #include <iostream>
 #include <string>
@@ -11,20 +11,19 @@ using namespace std;
 
 // Definitions of functions -- initialized below
 bool validateInput(vector<string>& vect, string sentence);
-Room moveForwardRoom(vector<Room>& vect, Room currentRoom);
-Room moveBackRoom(vector<Room>& vect, Room currentRoom);
+void moveForwardRoom(vector<Room>& vect, Room *&currentRoom);
+void moveBackRoom(vector<Room>& vect, Room *&currentRoom);
 
 int main() {
 	///////////////////////////////////////////////////////////////////////////////////////////
 	// Create all the variables and objects that will be needed throughout the game
 	///////////////////////////////////////////////////////////////////////////////////////////
 
-	// Version number of the game, will be output to console at start of game.
-	// Sentence that displays when the game ends.
-	string version = "Welcome to the Devil's Mansion V1.10\n";
+	// Various string variables that will be used throughout the game
+	string version = "Welcome to the Devil's Mansion V1.11\n";
 	string endSentence = "\nThanks for playing The Devil's Mansion";
 	string askUserToMove = "Please enter `move` to go through the door: ";
-	string startingDescription = "";
+	string startingDescription = ""; //Will be given a value after player has entered their name
 
 	// Loop variable that starts off as true. While the variable is true the mainloop will
 	// run the game and continuoulsy ask for user input.
@@ -41,11 +40,10 @@ int main() {
 
 	// Vector of created rooms that are currently not in use. A room object that will
 	// hold the current room the player is in. 
-	vector<Room> notInUseRooms;
+	vector<Room> rooms;
 
-	Room currentRoom; // Make this into a pointer somehow, Then we can make the 
-	// not in use rooms vector into a rooms vector and just point to the one currently
-	// being used
+	// Pointer variable that will point to the current room the player is in
+	Room *roomPointer = nullptr; 
 
 	// Create a game instance which will allow for user input and console output
 	// to be used.
@@ -63,8 +61,11 @@ int main() {
 	Room startingRoom("Starting Room",  "\nYou are now in the starting room.\n"
 										"The starting room is a large open dark room with creepy crawlers everywhere.\n"
 										"Standing before you is the devil\n", 1);
-	// Add the non starting rooms to the not in use rooms vector
-	notInUseRooms.push_back(startingRoom);
+	
+	// Add all of the rooms to the rooms vector. This will hold rooms that are 
+	// currently being used as well as rooms that are not currently being used
+	rooms.push_back(startingSteps);
+	rooms.push_back(startingRoom);
 
 	// Create the Devil who will be the main villain of the game. He will also be the one
 	// to give the player info on how to play the game.
@@ -75,6 +76,8 @@ int main() {
 						   "-----Enter better description of how to play the game-----\n"
 						   "That is the end of my spiel. Hopefully you can figure out the rest\n"
 						   "...The devil zoomed away\n");
+
+
 
 
 	///////////////////////////////////////////////////////////////////////////////////////////
@@ -93,7 +96,7 @@ int main() {
 	// Create the main player object and set the starting steps as their current room
 	Player player(name);
 	startingSteps.setPlayer(player);
-	currentRoom = startingSteps;
+	roomPointer = &startingSteps;
 
 	// Print the starting descriptions of the game to the screen. 
 	startingDescription = "\nHello there " + player.getName() + ".\n"
@@ -110,16 +113,14 @@ int main() {
 	// Once the user enters `move`, send them into the starting room and have the devil 
 	// give his little spiel about how the game works and runs.
 	if (input == "move") {
-		currentRoom = moveForwardRoom(notInUseRooms, currentRoom);
-		ui.printString(currentRoom.getRoomDescription());
+		moveForwardRoom(rooms, roomPointer);
+		ui.printString(roomPointer->getRoomDescription());
 		ui.printString(devil.getMonsterDescription());
 		ui.printString(devil.getDialogOpening());
 	}
-	
-	// Fix the starting room's description and reset the current room to account for this change
-	startingRoom.setRoomDescription("\nYou are now in the starting room.\n"
-									"The starting room is a large open dark room with creepy crawlers everywhere.\n");
-	currentRoom = startingRoom;
+
+
+
 
 	///////////////////////////////////////////////////////////////////////////////////////////
 	// Start the main loop of the game. The main loop will continuously ask the user for input.
@@ -138,17 +139,19 @@ int main() {
 		}
 		// Move the player to the 2nd room if they type move
 		else if (input == "move") {
-			currentRoom = moveForwardRoom(notInUseRooms, currentRoom);
-			ui.printString(currentRoom.getRoomDescription());
+			moveForwardRoom(rooms, roomPointer);
+			ui.printString(roomPointer->getRoomDescription());
 		}
 		// Move the player back to the starting room if they type back
 		else if (input == "back") {
-			currentRoom = moveBackRoom(notInUseRooms, currentRoom);
-			ui.printString(currentRoom.getRoomDescription());
+			moveBackRoom(rooms, roomPointer);
+			ui.printString(roomPointer->getRoomDescription());
 		}
 	}
 	ui.printString(endSentence);
 }
+
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -166,79 +169,64 @@ bool validateInput(vector<string>& vect, string sentence) {
 	return false;
 }
 
-// Switch the room the player is in. Take their object
-// from their current room and move them into the new room
-Room moveForwardRoom(vector<Room>& vect, Room currentRoom) {
-	// ToDo: Add validation to make sure the player can't go into a 
-	// room that doesn't exist
-
-	// Variables
-	Room tempRoom;
+// Switch the room the player is in forward one room. The function takes a vector
+// that is holding all of the different rooms and it also takes a pointer that is
+// pointing to the current room. The pointer will be switched to the new room the 
+// player is being moved to.
+void moveForwardRoom(vector<Room>& vect, Room *&currentRoom) {
+	// ToDo: Combine this with the moveBackRoom function
+	
 	Player defaultPlayer;
-	// Get the level of the next room. Level of the next room is
-	// whatever the level is of the current room plus one
-	int nextLevel = currentRoom.getRoomLevel() + 1;
+	// Variable to hold the room level that is one above the current room.
+	// This variable will be used to find the new room that the player will
+	// be moved to. It is plus one because they are moving forward.
+	int nextLevel = currentRoom->getRoomLevel() + 1;
 
-	// Set the temp room to the room the player is moving into. Also move 
-	// the player obejct to be in the new room
+	// Move the player to the new room, get rid of the player from the old room
+	// and move the pointer from the old room to the new room
 	for (int i = 0; i < vect.size(); i++) {
 		if (nextLevel == vect.at(i).getRoomLevel()) {
-			tempRoom = vect.at(i);
-			tempRoom.setPlayer(currentRoom.getPlayer());
-			vect.erase(vect.begin() + i);
+			// Move the player object from the currentRoom to the newRoom
+			vect.at(i).setPlayer(currentRoom->getPlayer());
+
+			// Clear the player from the old room
+			currentRoom->setPlayer(defaultPlayer);
+		
+			// Move the pointer from the old room to the new room
+			currentRoom = &vect.at(i);
 		}
 	}
-
-	// If there was no changes made, there was no higher room level found, then return
-	// the current room's object and display that you are still in the same room.
-	if (tempRoom.getPlayer().getName() != currentRoom.getPlayer().getName()) {
-		cout << "There is no room after this. You are still in the same room." << endl;
-		return currentRoom;
-	}
-	// Add the room the player is moving out of into the not in use rooms vector
-	currentRoom.setPlayer(defaultPlayer);
-	vect.push_back(currentRoom);
-
-	// Set the new room to be the current room
-	currentRoom = tempRoom;
-	return currentRoom;
 }
 
-// Switch the room the player is in. Take their object
-// from their current room and move them into the new room
-Room moveBackRoom(vector<Room>& vect, Room currentRoom) {
-	// ToDo: Add validation to make sure the player can't go into a 
-	// room that doesn't exist
+// Switch the room the player is in backwards one room. The function takes a vector
+// that is holding all of the different rooms and it also takes a pointer that is
+// pointing to the current room. The pointer will be switched to the new room the 
+// player is being moved to.
+void moveBackRoom(vector<Room>& vect, Room*& currentRoom) {
+	// ToDo: Combine this with the moveForwardRoom function
 
-	// Variables
-	Room tempRoom;
 	Player defaultPlayer;
-	// Get the level of the next room. Level of the next room is
-	// whatever the level is of the current room plus one
-	int nextLevel = currentRoom.getRoomLevel() - 1;
+	// Variable to hold the room level that is one below the current room.
+	// This variable will be used to find the new room that the player will
+	// be moved to. It is minus one because they are moving backwards.
+	int nextLevel = currentRoom->getRoomLevel() - 1;
 
-	// Set the temp room to the room the player is moving into. Also move 
-	// the player obejct to be in the new room
+	// Move the player to the new room, get rid of the player from the old room
+	// and move the pointer from the old room to the new room
 	for (int i = 0; i < vect.size(); i++) {
 		if (nextLevel == vect.at(i).getRoomLevel()) {
-			tempRoom = vect.at(i);
-			tempRoom.setPlayer(currentRoom.getPlayer());
-			vect.erase(vect.begin() + i);
+			// Move the player object from the currentRoom to the newRoom
+			vect.at(i).setPlayer(currentRoom->getPlayer());
+			//cout << "The name of the player in the new room is: " << vect.at(i).getPlayer().getName() << endl;
+
+			// Clear the player from the old room
+			currentRoom->setPlayer(defaultPlayer);
+			//cout << "The name of the player in the old room is: " << currentRoom->getPlayer().getName() << endl;
+
+			// Move the pointer from the old room to the new room
+			currentRoom = &vect.at(i);
+			//cout << "The name of the player in the new room is: " << currentRoom->getPlayer().getName() << endl;
+			//cout << "This was a check to make sure the pointer moved.\n";
 		}
 	}
-
-	// If there was no changes made, there was no lower room level found, then return
-	// the current room's object and display that you are still in the same room.
-	if (tempRoom.getPlayer().getName() != currentRoom.getPlayer().getName()) {
-		cout << "There is no room after this. You are still in the same room." << endl;
-		return currentRoom;
-	}
-
-	// Add the room the player is moving out of into the not in use rooms vector
-	currentRoom.setPlayer(defaultPlayer);
-	vect.push_back(currentRoom);
-
-	// Set the new room to be the current room
-	currentRoom = tempRoom;
-	return currentRoom;
 }
