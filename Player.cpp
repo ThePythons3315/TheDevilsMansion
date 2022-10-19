d938474a8c30c8eca78dc3abcca7a8dbbd3ec1b4
Shupperman <Jshup55@gmail.com>
2022-10-19 19:54:19 -0400

    Added a function to move an item from a room to the player's inventory.

--- Player.cpp
+++ Player.cpp
@@ -5,17 +5,25 @@ using namespace std;
 // Constructors
 Player::Player() {
 	name = "";
+	inventory;
 };
-Player::Player(string n) {
+Player::Player(string n, Inventory invent) {
 	name = n;
+	inventory = invent;
 }
 
 // Mutator Functions -- Functions that will be allowed to change private variables
 void Player::setName(string n) {
 	name = n;
 }
+void Player::setInventory(Inventory invent) {
+	inventory = invent;
+}
 
 // Accessor Functions -- Functions that will return values of private functions
 string Player::getName() {
 	return name;
+}
+Inventory Player::getInventory() {
+	return inventory;
 }
--- Player.h
+++ Player.h
@@ -4,20 +4,24 @@
 
 #include <iostream>
 #include <string>
+#include "Inventory.h"
 using namespace std;
 
 class Player {
 private:
 	string name;
+	Inventory inventory;
 public:
 	// Constructors
 	Player();
-	Player(string);
+	Player(string, Inventory);
 
 	// Mutator Functions -- Functions that will be allowed to change private variables
 	void setName(string);
+	void setInventory(Inventory);
 
 	// Accessor Functions -- Functions that will return values of private functions
 	string getName();
+	Inventory getInventory();
 };
 #endif // !PLAYER_H
