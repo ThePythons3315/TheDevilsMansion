ced123d2ab5247d77f43af3086c049a10dd708de
Shupperman <Jshup55@gmail.com>
2022-12-05 12:37:20 -0500

    Added: -When the player enters battle the text is changed to red. -When the player leaves battle the text is changed to white.

--- GameUI.h
+++ GameUI.h
@@ -11,4 +11,8 @@ public:
 	// Other functions
 	int writeOutput(std::string output);
 	std::string getUserInput(std::string prompt);
+
+	// Change the colors of the console and text
+	void changeToBattleColors();
+	void changeToNormalColors();
 };
